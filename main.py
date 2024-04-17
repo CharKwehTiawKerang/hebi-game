3ef89eac84d9bc379f0489513904aa59d3d65ba0
Hariz Asyraf <takamagahara16@gmail.com>
2024-04-17 17:20:35 +0800

    Added class and food, but still not working properly yet.

--- main.py
+++ main.py
@@ -1,37 +1,52 @@
 import pygame, sys
 from pygame.locals import *
+import constants
+import random
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-IN_GAME = True
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), RESIZABLE)
+# Global scope variables
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT), RESIZABLE)
 icon = pygame.image.load('redblazer.png')
 pygame.display.set_icon(icon)
 pygame.display.set_caption('Hebi Game')
+clock = pygame.time.Clock()
 
-player = pygame.Rect((300, 250, 25, 25))
+player = pygame.Rect((constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2, 20, 20))
+food = pygame.Rect((random.randint(0, 40) * constants.TILESIZE, random.randint(0, 30) * constants.TILESIZE, 20, 20))
 
-while IN_GAME:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            IN_GAME = False
+class Hebi:
+    def __init__(self):
+        self.length = 1
+        self.position = ((constants.SCREEN_WIDTH / 2), (constants.SCREEN_HEIGHT / 2))
 
-    screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (255, 0, 0), player)
+def main():
+    IN_GAME = True
 
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a] == True:
-        player.move_ip(-1, 0)
-    elif key[pygame.K_d] == True:
-        player.move_ip(1, 0)
-    elif key[pygame.K_w] == True:
-        player.move_ip(0, -1)
-    elif key[pygame.K_s] == True:
-        player.move_ip(0, 1)
+    while IN_GAME:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                IN_GAME = False
 
-    pygame.display.update()
+        x, y = screen.get_size()
+        screen.fill((constants.BLACK))
+        pygame.draw.rect(screen, (255, 0, 0), player)
+        pygame.draw.rect(screen, (200, 0, 100), food)
 
-pygame.quit()
+        key = pygame.key.get_pressed()
+        if key[pygame.K_a] == True and player.left > 0:
+            player.move_ip(-constants.TILESIZE, 0)
+        elif key[pygame.K_d] == True and player.right < constants.SCREEN_WIDTH:
+            player.move_ip(constants.TILESIZE, 0)
+        elif key[pygame.K_w] == True and player.top > 0:
+            player.move_ip(0, -constants.TILESIZE)
+        elif key[pygame.K_s] == True and player.bottom < constants.SCREEN_HEIGHT:
+            player.move_ip(0, constants.TILESIZE)
+
+        pygame.display.flip()
+        clock.tick(16) #Limit 10 FPS for smoother movements
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='hebi-game',
+    version='1.0.0',
+    author='Hariz Asyraf',
+    description='A 2D Snake Game',
+    packages=setuptools.find_packages()
+)
