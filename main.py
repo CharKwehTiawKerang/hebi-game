b758e543b9e455395ff195f5aa644acb7888c297
Hariz Asyraf <takamagahara16@gmail.com>
2024-04-18 21:58:25 +0800

    Add assets for food and snake, and also snake can eat food.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, os
 from pygame.locals import *
 import constants
 import random
@@ -6,14 +6,21 @@ import random
 pygame.init()
 
 # Global scope variables
-screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT), RESIZABLE)
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 icon = pygame.image.load('redblazer.png')
 pygame.display.set_icon(icon)
 pygame.display.set_caption('Hebi Game')
 clock = pygame.time.Clock()
 
+#assets from opengameart.org
+APPLE = pygame.image.load(os.path.join('assets', 'apple.png'))
+HEAD_UP = pygame.image.load(os.path.join('assets', 'head_up.png'))
+HEAD_DOWN = pygame.image.load(os.path.join('assets', 'head_down.png'))
+HEAD_RIGHT = pygame.image.load(os.path.join('assets', 'head_right.png'))
+HEAD_LEFT = pygame.image.load(os.path.join('assets', 'head_left.png'))
+
+
 player = pygame.Rect((constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2, 20, 20))
-food = pygame.Rect((random.randint(0, 40) * constants.TILESIZE, random.randint(0, 30) * constants.TILESIZE, 20, 20))
 
 class Hebi:
     def __init__(self):
@@ -23,25 +30,38 @@ class Hebi:
 def main():
     IN_GAME = True
 
+    food = pygame.Rect((random.randint(0, 40) * constants.TILESIZE, random.randint(0, 30) * constants.TILESIZE, 20, 20))
+    head_direction = HEAD_DOWN
+
     while IN_GAME:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 IN_GAME = False
 
-        x, y = screen.get_size()
+        offsetX, offsetY = screen.get_size()
         screen.fill((constants.BLACK))
-        pygame.draw.rect(screen, (255, 0, 0), player)
-        pygame.draw.rect(screen, (200, 0, 100), food)
+        screen.blit(head_direction, (player.x, player.y))
+        screen.blit(APPLE, (food.x, food.y))
 
         key = pygame.key.get_pressed()
         if key[pygame.K_a] == True and player.left > 0:
+            head_direction = HEAD_LEFT
             player.move_ip(-constants.TILESIZE, 0)
-        elif key[pygame.K_d] == True and player.right < constants.SCREEN_WIDTH:
+        elif key[pygame.K_d] == True and player.right < offsetX:
+            head_direction = HEAD_RIGHT
             player.move_ip(constants.TILESIZE, 0)
         elif key[pygame.K_w] == True and player.top > 0:
+            head_direction = HEAD_UP
             player.move_ip(0, -constants.TILESIZE)
-        elif key[pygame.K_s] == True and player.bottom < constants.SCREEN_HEIGHT:
+        elif key[pygame.K_s] == True and player.bottom < offsetY:
+            head_direction = HEAD_DOWN
             player.move_ip(0, constants.TILESIZE)
+
+        eat = pygame.Rect.colliderect(player, food)
+
+        if eat:
+            food.x = random.randint(0, 40) * constants.TILESIZE
+            food.y = random.randint(0, 30) * constants.TILESIZE
 
         pygame.display.flip()
         clock.tick(16) #Limit 10 FPS for smoother movements
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='hebi-game',
-    version='1.0.0',
+    version='1.0.1',
     author='Hariz Asyraf',
     description='A 2D Snake Game',
     packages=setuptools.find_packages()
