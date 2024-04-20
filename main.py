a7e8f6bc78508e175153e1fa2e2ba4117590d678
Hariz Asyraf <takamagahara16@gmail.com>
2024-04-20 21:25:21 +0800

    📝 Snake grows when eats food... but there is still another bug

--- main.py
+++ main.py
@@ -18,17 +18,46 @@ HEAD_UP = pygame.image.load(os.path.join('assets', 'head_up.png'))
 HEAD_DOWN = pygame.image.load(os.path.join('assets', 'head_down.png'))
 HEAD_RIGHT = pygame.image.load(os.path.join('assets', 'head_right.png'))
 HEAD_LEFT = pygame.image.load(os.path.join('assets', 'head_left.png'))
+BODY_VERTICAL = pygame.image.load(os.path.join('assets', 'body_vertical.png'))
 
+# def update_body():
+#     if len(snake_body) > 0:
+#         for i in range(len(snake_body) -1, 0, -1):
+#             snake_body[i].x = snake_body[i - 1].x
+#             snake_body[i].y = snake_body[i - 1].y
+        
+#         snake_body[0].x = (player_head.x + 20)
+#         snake_body[0].y = (player_head.y + 20)
 
-player = pygame.Rect((constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2, 20, 20))
-
-class Hebi:
+class Snake:
     def __init__(self):
-        self.length = 1
-        self.position = ((constants.SCREEN_WIDTH / 2), (constants.SCREEN_HEIGHT / 2))
+        self.head = pygame.Rect((constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2, 20, 20))
+        self.body = []
+        self.body.append(self.head)
+
+    def move(self, dx, dy):
+        self.head.move_ip(dx, dy)
+        for i in range(len(self.body) - 1, 0, -1):
+            self.body[i] = self.body[i - 1].copy()
+
+    def grow(self):
+        last_segment = self.body[-1]
+        new_segment = pygame.Rect(last_segment.x, last_segment.y, 20, 20)
+        self.body.append(new_segment)
+
+    def draw(self, surface, head_direction):
+        for i, segment in enumerate(self.body):
+            if i == 0:
+                continue
+            surface.blit(BODY_VERTICAL, segment)
+
+        surface.blit(head_direction, self.head)
+
 
 def main():
     IN_GAME = True
+
+    hebi = Snake()
 
     food = pygame.Rect((random.randint(0, 40) * constants.TILESIZE, random.randint(0, 30) * constants.TILESIZE, 20, 20))
     head_direction = HEAD_DOWN
@@ -40,28 +69,30 @@ def main():
 
         offsetX, offsetY = screen.get_size()
         screen.fill((constants.BLACK))
-        screen.blit(head_direction, (player.x, player.y))
+
+        hebi.draw(screen, head_direction)
         screen.blit(APPLE, (food.x, food.y))
 
         key = pygame.key.get_pressed()
-        if key[pygame.K_a] == True and player.left > 0:
+        if key[pygame.K_a] == True and hebi.head.left > 0:
             head_direction = HEAD_LEFT
-            player.move_ip(-constants.TILESIZE, 0)
-        elif key[pygame.K_d] == True and player.right < offsetX:
+            hebi.move(-constants.TILESIZE, 0)
+        elif key[pygame.K_d] == True and hebi.head.right < offsetX:
             head_direction = HEAD_RIGHT
-            player.move_ip(constants.TILESIZE, 0)
-        elif key[pygame.K_w] == True and player.top > 0:
+            hebi.move(constants.TILESIZE, 0)
+        elif key[pygame.K_w] == True and hebi.head.top > 0:
             head_direction = HEAD_UP
-            player.move_ip(0, -constants.TILESIZE)
-        elif key[pygame.K_s] == True and player.bottom < offsetY:
+            hebi.move(0, -constants.TILESIZE)
+        elif key[pygame.K_s] == True and hebi.head.bottom < offsetY:
             head_direction = HEAD_DOWN
-            player.move_ip(0, constants.TILESIZE)
+            hebi.move(0, constants.TILESIZE)
 
-        eat = pygame.Rect.colliderect(player, food)
+        eat = pygame.Rect.colliderect(hebi.head, food)
 
         if eat:
             food.x = random.randint(0, 40) * constants.TILESIZE
             food.y = random.randint(0, 30) * constants.TILESIZE
+            hebi.grow()
 
         pygame.display.flip()
         clock.tick(16) #Limit 10 FPS for smoother movements
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='hebi-game',
-    version='1.0.1',
+    version='1.0.2',
     author='Hariz Asyraf',
     description='A 2D Snake Game',
     packages=setuptools.find_packages()
